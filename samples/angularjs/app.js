ea3ba13b09bb2649a98921060a026c4ed86754d9
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2014-09-24 12:56:59 -0300

    Samples.

---
+++ samples/angularjs/app.js
@@ -0,0 +1,17 @@
+angular.module('App', []).controller('MainController', ['$scope', '$sce', '$http', function ($scope, $sce, $http) {
+    $scope.result = null;
+
+    $http({ method: 'GET', url: 'http://www.gistfy.com/github/isagalaev/highlight.js/test/detect/python/default.txt?type=html&lang=python' })
+        .success(function (data, status, headers, config) {
+            $scope.result = $sce.trustAsHtml(data);
+        })
+        .error(function (data, status, headers, config) {
+            if (status === 400 || status === 412) {
+                $scope.result =  $sce.trustAsHtml(data);
+            } else if (status === 404) {
+                $scope.result = $sce.trustAsHtml('<strong>' + $scope.isGist ? 'Gist not found.' : 'File not found.' + '</strong>');
+            } else {
+                $scope.result = $sce.trustAsHtml('<strong>Oh snap! There\'s an error.</strong>');
+            }
+        });
+}]);
